b052c6609dc44d4c8e6ac80332ee60395bcb69df
Spinoza235 <esperancedingammadji785@gmail.com>
2025-06-02 09:20:19 +0200

    new moif

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -10,6 +10,24 @@
                 <li><a href="{{ route('home') }}#medicaments" class="scroll-link">Médicaments</a></li>
                 <li><a href="{{ route('home') }}#services" class="scroll-link">Services</a></li>
                 <li><a href="{{ route('contact') }}">Contact</a></li>
+                <li>
+    <a href="" 
+       style="background-color: #ff4444; 
+              color: white; 
+              padding: 5px 8px; 
+              border-radius: 4px; 
+              font-weight: bold;
+              transition: all 0.3s ease;
+              display: inline-block;
+              text-align: center;
+              text-decoration: none;
+              border: none;
+              cursor: pointer;"
+             onmouseover="this.style.backgroundColor='#cc0000'" 
+             onmouseout="this.style.backgroundColor='#ff4444'">
+             Déconnexion
+    </a>
+</li>
             </ul>
             <div class="hamburger">
                 <span class="bar"></span>
